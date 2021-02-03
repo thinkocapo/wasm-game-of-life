0c5412d6f5841438f3081f8c9cfae7ef26322bc5
William Capozzoli <thinkocapo@gmail.com>
2021-02-03 18:22:26 -0500

    sentry-browser sdk installed, greeting updated

--- www/bootstrap.js
+++ www/bootstrap.js
@@ -1,3 +1,9 @@
+Sentry.init({
+  dsn: "https://4aa70d5c35d0429fa1b31c1112c118ea@o262702.ingest.sentry.io/5622146",
+  release: "0.1.0",
+  environment: "development"
+})
+
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
